a96551fe8d14a1ee175fb1f2bac26a09bbe33357
sreekanthThadigot <sreekanth@ampcome.com>
2024-06-13 15:40:23 +0530

    change port

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ app.get("/", async (req,res)=>{
     res.send("Welcome to my API");
 })
 
+
+app.get("/crash", async (req,res)=>{
+    
+    console.log("chrashing details ",req,res);
+
+    if(true){
+        throw new Error("it is going to crash")
+    }
+    res.send("Welcome to my API");
+})
+
+
 app.listen(3000, ()=>{
     console.log("app listening on port 3000")
 })
